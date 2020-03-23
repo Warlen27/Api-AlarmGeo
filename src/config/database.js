1bea64490bcc6cfefbc84d96c4c5e83f5a9b3487
Warlen27 <warlenalves40@gmai.com>
2020-03-23 04:49:31 -0300

    exchanging database.

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,12 @@
+
 module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
     password: '******',
-    database: 'dbAGP',
-    difine: {
-        timestamps: true,
-        underscored: true, //utiliza o formato Snake case | default pascal case.
+    database: 'dbAlarmGeo',
+    define: {
+      timestamps: true,
+      underscored: true, //utiliza o formato Snake case | default pascal case.
     },
-};
+  };
