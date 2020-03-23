e0ee936499eed32ea25cdbdf1a8e9c3d3b624fd8
Warlen27 <warlenalves40@gmai.com>
2020-03-23 04:56:22 -0300

    pointing folder to upload files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
+const path = require('path');
+const cors = require('cors');
 const routes = require('./routes');
 
-const app = express();
-app.use(express.json());
+require('./database');
 
+const app = express();
+
+app.use(cors());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')));
+app.use(express.json());
 app.use(routes);
 
-app.listen(8080);
+app.listen(3333)
