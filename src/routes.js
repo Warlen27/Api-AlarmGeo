40138ec1466182dd0a6d71df2e6fa7981dc71771
Warlen27 <warlenalves40@gmai.com>
2020-03-23 04:53:40 -0300

    configuring routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const multer = require('multer');
+
+
+const uploadConfig = require('./config/upload');
+const UserController = require('./controllers/UserController');
+const SessionControler = require('./controllers/SessionController');
+const PlaceController = require('./controllers/PlaceController');
+
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.get('/', (req, res) => {
-    return  res.json({hello: "world" });
-})
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+routes.post('/login', SessionControler.store);
+routes.patch('/users/:user_id/update', upload.single('avatar'), UserController.update);
+
+routes.get('/users/:user_id/places', PlaceController.index);
+routes.post('/users/:user_id/places', upload.single('thumbnail'), PlaceController.store);
+routes.delete('/users/:place_id/places', PlaceController.destroy);
+
 
 module.exports = routes;
