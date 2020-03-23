ffc6e62e58d848a4558ba3d7dac61ffad6c0c333
Warlen27 <warlenalves40@gmai.com>
2020-03-23 04:51:49 -0300

    associating the models

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,17 @@
 const Sequelize = require('sequelize');
-
 const dbConfig = require('../config/database');
 
-const connection =  new Sequelize(dbConfig);
+const User = require('../models/User');
+const Place = require('../models/Place');
+
+
+const connection = new Sequelize(dbConfig);
+
+User.init(connection);
+Place.init(connection);
+
+Place.associate(connection.models);
+User.associate(connection.models);
+
 
 module.exports = connection;
